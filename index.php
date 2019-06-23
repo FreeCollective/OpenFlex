33ebe1817da043ecdc2d6804a9d0168ea9913cf2
illia <illialoo99@gmail.com>
2019-06-24 02:49:14 +0300

    Added getAudio method

--- index.php
+++ index.php
@@ -5,6 +5,12 @@ case "getInfo":
 $json_infoRequest = json_encode("$infoRequest");
 echo("$json_infoRequest");
 break;
+case "getAudio":
+/* Maybe this doesn't work correctly
+if(isset($_GET['audios'])) {
+Maybe $_GET variables on isset will always send TRUE*/
+curl("https://api.vk.com/method/audio.getById?access_token=$token&audios=$_GET['audios']&v=5.92");
+break;
 case "":
 echo("Method required");
 break;
