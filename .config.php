19e7e0365d73b58a02bb58d39f89582588e82efa
illia <illialoo99@gmail.com>
2019-06-24 00:43:04 +0300

    Base config for main

---
+++ .config.php
@@ -0,0 +1,8 @@
+<?php
+#change this values
+$donate_url = "http://example.com";
+$ads_url = "http://example.com"
+
+#this is responce
+$infoRequest = array("donate_url" => "$donate_url", "ads_url" => "$ads_url");
+?>
