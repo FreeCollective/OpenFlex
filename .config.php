c7e20bb5636578272274eaa3fe5dbb5c6899e6e8
illia <illialoo99@gmail.com>
2019-06-24 01:01:29 +0300

    Update .config.php

--- .config.php
+++ .config.php
@@ -1,8 +1,10 @@
 <?php
 #change this values
 $donate_url = "http://example.com";
-$ads_url = "http://example.com"
+$ads_url = "http://example.com/api.php"
+  
+$operationFinish = FALSE;
 
-#this is responce
+#this is main responce
 $infoRequest = array("donate_url" => "$donate_url", "ads_url" => "$ads_url");
 ?>
