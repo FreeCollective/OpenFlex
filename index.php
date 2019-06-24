463157fc2a56548c0bd0d4d23ead423690e79761
illia <illialoo99@gmail.com>
2019-06-24 04:22:39 +0300

    small update

--- index.php
+++ index.php
@@ -2,15 +2,21 @@
 require_once ".config.php"
 switch($_GET["method"]) {
 case "getInfo":
-$json_infoRequest = json_encode("$infoRequest");
-echo("$json_infoRequest");
+$responce = json_encode("$infoRequest");
+echo("$responce");
 break;
 case "getAudio":
 /* Maybe this doesn't work correctly
 if(isset($_GET['audios'])) {
 Maybe $_GET variables on isset will always send TRUE*/
-curl("https://api.vk.com/method/audio.getById?access_token=$token&audios=$_GET['audios']&v=5.92");
+$request = curl("https://api.vk.com/method/audio.getById?access_token=$token&audios=$_GET['audios']&v=5.92");
+echo $request;
 break;
+case "loadTrack":
+$proxymsg = ("message" => "This doesn't work at this time");
+//curl("https://vk.com/method.audio.get?audio=$_GET['track']&access_token=$token&v=5.92");
+$responce = json_encode($proxymsg);
+echo $responce;
 case "":
 echo("Method required");
 break;
